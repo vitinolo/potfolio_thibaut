f77edd584cf97bd5dcca984b3deb0ac4ffbfcbd9
thibaut lenoir <lenoir677@gmail.com>
2023-02-09 12:15:07 +0100

    About

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,9 +8,9 @@ const About = () => {
             <VisibilityNav />
             <Social />
             <div className='aboutContent'>
-                <h1>En perpétuelle recherche de nouveaux défis et de nouvelles compétences pour réaliser des projets toujours plus aboutis.</h1>
+                <h1>En perpétuelle recherche de nouvelles compétences pour réaliser des projets toujours plus aboutis.</h1>
                 <div className="wrapper">
-                    <div className='languages'>
+                    <div className='languages card'>
                         <h1>Langages utilisés :</h1>
                         <div className="item"><i class="fab fa-css3-alt"></i> Css3</div>
                         <div className="item"><i class="fab fa-html5"></i> Html5</div>
@@ -18,34 +18,33 @@ const About = () => {
                         <div className="item"><i class="fab fa-react fa-spin"></i> React.js</div>
                         <div className="item"><i class="fab fa-node-js"> Node.js</i></div>
                     </div>
-                    <div className='skills'>
+                    <div className='skills card'>
                         <h1>Skills :</h1>
-                        <div className="item"><i class="fab fa-github"> Github</i></div>
+                        <div className="item">Github</div>
                         <div className="item">SEO</div>
                         <div className="item">MongoDB</div>
                         <div className="item">Vscode</div>
                         <div className="item">Postman</div>
                         <div className="item">Anglais</div>
                     </div>
-                    <div className='hobbies'>
-                        <h1>Hobbies :</h1>
-                        <div className="item"><i class="fas fa-code"> Code</i></div>
-                        <div className="item"><i class="fas fa-music"> Piano</i></div>
-                        <div className="item"><i class="fas fa-palette"> Peinture</i></div>
-                        <div className="item"><i class="fas fa-hiking"> Randonnée</i></div>
-                        <div className="item">Touring</div>
+                    <div className='softSkills card'>
+                        <h1>Soft skills :</h1>
+                        <div className="item">Code</div>
+                        <div className="item">Piano</div>
+                        <div className="item">Peinture</div>
+                        <div className="item">Randonnée</div>
                     </div>
                 </div>    
             </div>
             <div class='light x1'></div>
-                    <div class='light x2'></div>
-                    <div class='light x3'></div>
-                    <div class='light x4'></div>
-                    <div class='light x5'></div>
-                    <div class='light x6'></div>
-                    <div class='light x7'></div>
-                    <div class='light x8'></div>
-                    <div class='light x9'></div>
+            <div class='light x2'></div>
+            <div class='light x3'></div>
+            <div class='light x4'></div>
+            <div class='light x5'></div>
+            <div class='light x6'></div>
+            <div class='light x7'></div>
+            <div class='light x8'></div>
+            <div class='light x9'></div>
         </div>
     );
 };
