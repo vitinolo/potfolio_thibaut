36dfcb0d1752e2636f3da8f9b2cb1460071691d8
thibaut lenoir <lenoir677@gmail.com>
2023-02-14 19:37:54 +0100

    Projects

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,20 @@
 import React from 'react';
 
-function Card ({type, name, languages, languagesIcons, source, info})  {
+function Card ({type, picture, name, languages, source, info})  {
     return (
         <div className='projectContent'>
-            <h1>{name}</h1>
-            <span>Type: {type}</span>
-            <div className='languages'>Langages: {languages}</div>
-            <div className="languagesIcons">{languagesIcons}</div>
-            <div className="source"><a href={source} title='cliquer vers code source'>Code source</a></div>
-            <div className="info">Info: {info}</div>
+            <>
+                <div className='wrapper'>
+                    <h1>{name}</h1>
+                    <img src={picture} width="300px" height="150px" alt="projet" />
+                </div>
+                <div className='wrapper2'>
+                    <div className='type'>Type de projet: {type}</div>
+                    <div className='languages'>Langages: {languages}</div>
+                    <div className="source"><a href={source} title='cliquer vers code source'>Code source</a></div>
+                    <div className="info">Info: {info}</div>
+                </div>
+            </>
         </div>    
     );
 };
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,78 +2,82 @@ import React from 'react';
 import Card from './Card';
 
 function ProjectList () {
+
   const projects = [
       {
         id: 1,
-        type: 'school project',
         name: 'Booki',
-        languages: ['html css'],
-        languagesIcons: ['fab fa-html5','fab fa-css3-alt'],
+        picture: 'img/p2_portfolio.png',
+        type: "école.",
+        languages: ['html css.'],
         source: 'https://github.com/vitinolo/thibautlenoir_2_20092021',
-        info: 'Transformez une maquette en site web avec HTML & CSS', 
+        info: 'Transformez une maquette en site web avec HTML & CSS.', 
       },
       {
         id: 2,
-        type: 'school project',
         name: 'Ohmyfood',
-        languages: ['html css'],
-        languagesIcons: ['fab fa-css3-alt', 'fab fa-github', 'fab fa-sass'],
+        picture: '/img/p3_portfolio.png',
+        type: "école.",
+        languages: ['html css.'],
         source: 'https://github.com/vitinolo/ThibautLenoir_3_20112021',
         info: 'Dynamisez une page web avec des animations CSS.',
       },
       {
         id: 3,
-        type: 'school project',
         name: 'La chouette',
-        languages: ['SEO css'],
-        languagesIcons: ['fab fa-html5','fab fa-css3-alt'],
+        picture: '/img/p4_portfolio.png',
+        type: "école.",
+        languages: ['SEO css.'],
         source: 'https://github.com/vitinolo/P4',
         info: 'Optimisez un site web existant.',
       },
       {
         id: 4,
         name: 'Kanap',
-        type: 'school project',
-        languages: ['javascript API'],
-        languagesIcons: ['fab fa-js'],
+        picture: '/img/p5_portfolio.png',
+        type: "école.",
+        languages: ['javascript API.'],
         source: 'https://github.com/vitinolo/p5_kanap',
         info: 'Construisez un site e-commerce en JavaScript.',
       },
       {
         id: 5,
         name: 'Piiquantes',
-        type: 'school project',
-        languages: ['backend API'],
-        languagesIcons: ['fab fa-js','fab fa-node-js', 'fab fa-css3-alt'],
+        picture: '/img/p6_portfolio.webp',
+        type: "école.",
+        languages: ['backend API.'],
         source: 'https://github.com/vitinolo/p6',
         info: "Construisez une API sécurisée pour une application d'avis gastronomiques.",
       },
       {
         id: 6,
         name: 'Groupomania',
-        type: 'school project',
-        languages: ['javascript react css'],
-        languagesIcons: ['fab fa-js','fab fa-react','fab fa-css3-alt'],
+        picture: '/img/p7_portfolio.webp',
+        type: "école.",
+        languages: ['javascript react css.'],
         source: 'https://github.com/vitinolo/P7_Creez_un_reseau_social_d_entreprise',
         info: "Créez un réseau social d'entreprise.",
       },
       {
         id: 7,
         name: 'Sky drone air',
-        type: 'own project',
-        languages: ['javascript react css'],
-        languagesIcons: ['fab fa-js','fab fa-react','fab fa-css3-alt'],
+        picture: '/img/skydroneair_portfolio.png',
+        type: 'individuel.',
+        languages: ['javascript react css.'],
         source: 'https://skydroneair.netlify.app/',
-        info: 'Site drône.',
+        info: "Création d'un site de prise de vue par drone.",
       },
     ];
     return (
-        <div className='projectList'>  
+      <>
+        <h1>Projets</h1> 
+        <div className='projectList'> 
             { projects.map((project) =>(
                 <Card 
                     key={project.id}
                     name={project.name}
                     type={project.type}
+                    picture={project.picture}
                     languages={project.languages}
                     languagesIcon={project.languagesIcons}
                     source={project.source}
@@ -82,6 +86,7 @@ function ProjectList () {
               ))
             }        
         </div>
+      </>
     );
 };
 
